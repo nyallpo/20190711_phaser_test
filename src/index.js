c63547677233bb24b6b98d2999f89e1d220003e9
nyallpo <nyallpo@example.com>
2019-07-11 21:06:48 +0900

    add dude

--- src/index.js
+++ src/index.js
@@ -1,32 +1,35 @@
-import Phaser from "phaser";
-import logoImg from "./assets/logo.png";
+import Phaser from "phaser"
+import logoImg from "./assets/logo.png"
+import dudeImg from './assets/dude.png'
 
 const config = {
   type: Phaser.AUTO,
-  parent: "phaser-example",
+  parent: "game",
   width: 800,
   height: 600,
   scene: {
     preload: preload,
     create: create
   }
-};
+}
 
-const game = new Phaser.Game(config);
+const game = new Phaser.Game(config)
 
 function preload() {
-  this.load.image("logo", logoImg);
+  this.load.image("logo", logoImg)
+  this.load.image('dude', dudeImg)
 }
 
 function create() {
-  const logo = this.add.image(400, 150, "logo");
+  const logo = this.add.image(400, 150, "logo").setScale(0.2)
+  const dude = this.add.image(200, 100, 'dude')
 
   this.tweens.add({
     targets: logo,
     y: 450,
     duration: 2000,
     ease: "Power2",
-    yoyo: true,
+    yoyo: false,
     loop: -1
   });
 }
